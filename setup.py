99f8df7f9ef263c09cbd32b5528da1be3e4bbaf1
bedekelly <bedekelly97@gmail.com>
2014-06-14 17:04:35 +0100

    Made it PyPI installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from distutils.core import setup
+setup(name='SimpleMenu',
+	  version='1.0.1',
+	  py_modules=['SimpleMenu'],
+	  description="Create simple Curses menus",
+	  author="Bede Kelly",
+	  author_email="bedekelly97@gmail.com",
+	  url="https://github.com/bedekelly/simple-menu",
+	  provides=['SimpleMenu'])
